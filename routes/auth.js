51ae76ba37c380e505bbd13cd21b01b5a34942bc
mathieu.jee <mathieu.jee@heig-vd.ch>
2018-12-06 13:16:10 +0100

    User token generation + verification

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const { jwtOptions } = require('../config');
+
+const USER = {
+    id: '123456789',
+    email: 'toto@tata.com',
+    username: 'toto',
+    password: 'tata'
+}
 
 const router = express.Router();
 
+
+
 router.post('/login', (req, res) => {
     const { username, password } = req.body;
-    res.send({ username, password: '*'.repeat(password.length)});
+
+    if(username === USER.username && password === USER.password) {
+        const token = jwt.sign({ userID: USER.id }, jwtOptions.secret);
+        const decoded = jwt.verify(token, jwtOptions.secret);
+        return res.send({ token, decoded });
+    }
+    return res.sendStatus(401);
 });
 
 module.exports = router;
