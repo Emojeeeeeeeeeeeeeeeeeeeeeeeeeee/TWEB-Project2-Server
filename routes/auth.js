221fb0c694f7c7caaf97515b980a257e42ac5472
olivier_kopp <olivier.kopp@heig-vd.ch>
2018-12-13 14:38:20 +0100

    +++++------

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,36 @@ const passportLocal = require('passport-local');
 const passportJWT = require('passport-jwt');
 const jwt = require('jsonwebtoken');
 const { jwtOptions } = require('../config');
+const mongoose = require('mongoose');
+
+const dbURI = 'mongodb://admin:<PASSWORD>@twebkoppsilvestri-shard-00-00-y2dgh.mongodb.net:27017,twebkoppsilvestri-shard-00-01-y2dgh.mongodb.net:27017,twebkoppsilvestri-shard-00-02-y2dgh.mongodb.net:27017/test?ssl=true&replicaSet=TWEBKoppSilvestri-shard-0&authSource=admin&retryWrites=true';
+
+const options = {
+    useNewUrlParser: true,
+    dbName: 'HappyFaces',
+  };
+  
+  mongoose.connect(dbURI, options);
+  
+  const userSchema = new mongoose.Schema();
+
+  const messageSchema = new mongoose.Schema({
+    content: { type: String, required: true },
+    likes: { type: [userSchema], required: true },
+    time: { type: Date, required: true }
+  });
+
+  const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    messages: { type: [messageSchema], required: true },
+    followed: { type: [userSchema], required: true },
+    followers: { type: [userSchema], required: true }
+  });
+
+const UserModel = mongoose.model('user', userSchema);
+const MessageModel = mongoose.model('message', messageSchema);
 
 const USER = {
     id: '123456789',
@@ -26,11 +56,9 @@ passport.use(new LocalStrategy(
     },
     // Verification function
     (email, password, done) => {
-        // TODO: DB query
-        if(email === USER.email && password === USER.password) {
-            return done(null, USER);
-        }
-        return done(null, false);
+        let USER;
+        UserModel.findOne({ email: email, password: password }, {password : 0}, function(err, obj) {if(err || !obj){console.log(obj)}else{console.log(obj)}})
+        UserModel.findOne({ email: email, password: password }, function(err, obj) {if(err || !obj){return done(null, false)}else{return done(null, obj)}})
     }
 ));
 
