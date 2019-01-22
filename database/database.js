024f28a3d8867d0e047f9dcfeef17163c9249fe1
unknown <olivier.kopp@heig-vd.ch>
2019-01-22 22:25:43 +0100

    updates

--- database/database.js
+++ database/database.js
@@ -3,31 +3,31 @@ const mongoose = require('mongoose');
 const dbURI = 'mongodb://user1:user1@twebkoppsilvestri-shard-00-00-y2dgh.mongodb.net:27017,twebkoppsilvestri-shard-00-01-y2dgh.mongodb.net:27017,twebkoppsilvestri-shard-00-02-y2dgh.mongodb.net:27017/test?ssl=true&replicaSet=TWEBKoppSilvestri-shard-0&authSource=admin&retryWrites=true';
 
 const options = {
-    useNewUrlParser: true,
-    dbName: 'HappyFaces',
-  };
-  
-  mongoose.connect(dbURI, options);
-  
-  const ObjectId = mongoose.Types.ObjectId;
+  useNewUrlParser: true,
+  dbName: 'HappyFaces',
+};
+
+mongoose.connect(dbURI, options);
+
+const { ObjectId } = mongoose.Types;
 
 
-  const messageSchema = new mongoose.Schema({
-    authorId: {type: String, required: true},
-    content: { type: String, required: true },
-    like: { type: Array, required: true, default: [] },
-    timestamp: { type: Date, required: true, default: Date.now }
-  });
+const messageSchema = new mongoose.Schema({
+  authorId: { type: String, required: true },
+  content: { type: String, required: true },
+  like: { type: Array, required: true, default: [] },
+  timestamp: { type: Date, required: true, default: Date.now },
+});
 
-  const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true },
-    messages: { type: [String], required: true, default: [] },
-    following: { type: [String], required: true, default: [] },
-    followers: { type: [String], required: true, default: [] },
-    image: {type: String, required: true, default: "https://cdn.shopify.com/s/files/1/1061/1924/products/Nerd_with_Glasses_Emoji_2a8485bc-f136-4156-9af6-297d8522d8d1_large.png?v=1483276509"}
-  });
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  messages: { type: [String], required: true, default: [] },
+  following: { type: [String], required: true, default: [] },
+  followers: { type: [String], required: true, default: [] },
+  image: { type: String, required: true, default: 'https://cdn.shopify.com/s/files/1/1061/1924/products/Nerd_with_Glasses_Emoji_2a8485bc-f136-4156-9af6-297d8522d8d1_large.png?v=1483276509' },
+});
 
 const UserModel = mongoose.model('user', userSchema);
 const MessageModel = mongoose.model('message', messageSchema);
--- graphQL/graphQL.js
+++ graphQL/graphQL.js
@@ -1,6 +1,6 @@
 const { buildSchema } = require('graphql');
 const { UserModel, MessageModel, ObjectId } = require('../database/database');
-const {images} = require('../resources/DefaultAvatars')
+const { images } = require('../resources/DefaultAvatars');
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
@@ -48,355 +48,305 @@ const schema = buildSchema(`
 
 // The root provides a resolver function for each API endpoint
 const root = {
-  getMessagesFromDB: ({ authorId, offset}) => {
-    return new Promise((resolve, reject) => {
-    UserModel.findOne({_id : authorId}, {email : 1, following : 1, _id : 1}).then((data) => {
-      userFollowedTab = data.following === undefined ? [] : data.following;
+  getMessagesFromDB: ({ authorId, offset }) => new Promise((resolve, reject) => {
+    UserModel.findOne({ _id: authorId }, { email: 1, following: 1, _id: 1 }).then((data) => {
+      const userFollowedTab = data.following === undefined ? [] : data.following;
       userFollowedTab.push(data.id);
 
-      const promises= [];
-      messages = [];
-      userFollowedTab.forEach(element => {
-        promises.push(MessageModel.find({authorId: element}));
+      const promises = [];
+      userFollowedTab.forEach((element) => {
+        promises.push(MessageModel.find({ authorId: element }));
       });
-      Promise.all(promises).then((data) => {
-          let fullData = []
-          data.forEach(element => {
-            fullData = fullData.concat(element);
-          })
-          if(fullData.length === 0){
-            resolve(null);
-          }
-        else{
-          fullData.sort(function(a, b){
-          return b.timestamp-a.timestamp;
+      Promise.all(promises).then((data2) => {
+        let fullData = [];
+        data2.forEach((element) => {
+          fullData = fullData.concat(element);
         });
-        resolve(fullData.slice((offset*999) + offset, (offset + 1) * 999));
-        
-      }
+        if (fullData.length === 0) {
+          resolve(null);
+        } else {
+          fullData.sort((a, b) => b.timestamp - a.timestamp);
+          resolve(fullData.slice((offset * 999) + offset, (offset + 1) * 999));
+        }
       })
-      .catch(err => {
-        reject(err);
-      })
-
-    })
-    .catch(err => {
-      reject(err);
-    })
-  })
-  },
-  getMessagesOfUser: ( { userId, offset }) => {
-    return new Promise((resolve, reject) => {
-    UserModel.findOne({_id : userId})
-    .then((data) => {
-      if(data === null){
-        resolve(null)
-      }
-      else{
-        MessageModel.find({authorId : userId})
-        .then(messages => {
-          let fullData = messages;
-          fullData.sort(function(a, b){
-          return b.timestamp-a.timestamp;
-        });
-          resolve(fullData.slice((offset*999) + offset, (offset + 1) * 999))
-        })
-        .catch(err => {
+        .catch((err) => {
           reject(err);
-        })
-      }
-    })
-    .catch(err => {
-      reject(err)
-    })
-  })
-  },
-  getFavoriteMessages: ( { userId, offset }) => {
-    return new Promise((resolve, reject) => {
-    UserModel.findOne({_id : userId}, {email : 1, following : 1, _id : 1})
-    .then((data) => {
-      userFollowedTab = data.following === undefined ? [] : data.following;
-      userFollowedTab.push(data.id);
-
-      const promises= [];
-      messages = [];
-      userFollowedTab.forEach(element => {
-        promises.push(MessageModel.find({authorId: element, like : userId}));
-      });
-      Promise.all(promises).then((data) => {
-          let fullData = []
-          data.forEach(element => {
-            fullData = fullData.concat(element);
-          })
-          if(fullData.length === 0){
-            resolve(null);
-          }
-        else{
-          fullData.sort(function(a, b){
-          return b.timestamp-a.timestamp;
         });
-        resolve(fullData.slice((offset*999) + offset, (offset + 1) * 999));
-        
-      }
-      })
-
     })
-    .catch(err => {
-      reject(err);
-    })
-  })
-  },
-  createMessage: ({ authorId, content }) => {
-    return new Promise((resolve, reject) => {
-    let newMessage = new MessageModel({ content: content, authorId: authorId});
-    newMessage.save(function(err, message){
-      const id = message.id;
-      UserModel.updateOne({_id: authorId}, {$addToSet: {messages: id}})
-      .then(res => {
-        resolve(message);
-        })
-      .catch(err => {
-        reject(err)
-      })
-    });
-  })
-  },
-  deleteMessage: ({messageId, authorId}) => {
-    return new Promise((resolve, reject) => {
-    MessageModel.deleteOne({_id : ObjectId(messageId)})
-    .then(res => {
-      UserModel.updateOne( {_id: authorId}, { $pull: { "messages" : { id: ObjectId(messageId) } } }, false)
-      .then(res => {
-        resolve(true);
+      .catch((err) => {
+        reject(err);
       });
-    })
-    .catch(err => {
-      reject(err);
-    })
-  })
-  },
-  follow: ({targetId, userId}) => {
-    return new Promise((resolve, reject) => {
-      const promises = []
-     //add the user to the followers of the target
-     promises.push(UserModel.updateOne({_id: targetId}, {$addToSet: {followers: userId}}))
-     //add the target to the followed list of the current user
-     promises.push(UserModel.updateOne({_id : userId}, {$addToSet : {following: targetId}}))
-     Promise.all(promises)
-     .then(res => {
-       resolve(true)
-     })
-     .catch(err => {
-       reject(err)
-     })
+  }),
+  getMessagesOfUser: ({ userId, offset }) => new Promise((resolve, reject) => {
+    UserModel.findOne({ _id: userId })
+      .then((data) => {
+        if (data === null) {
+          resolve(null);
+        } else {
+          MessageModel.find({ authorId: userId })
+            .then((messages) => {
+              const fullData = messages;
+              fullData.sort((a, b) => b.timestamp - a.timestamp);
+              resolve(fullData.slice((offset * 999) + offset, (offset + 1) * 999));
+            })
+            .catch((err) => {
+              reject(err);
+            });
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  }),
+  getFavoriteMessages: ({ userId, offset }) => new Promise((resolve, reject) => {
+    UserModel.findOne({ _id: userId }, { email: 1, following: 1, _id: 1 })
+      .then((data) => {
+        const userFollowedTab = data.following === undefined ? [] : data.following;
+        userFollowedTab.push(data.id);
+
+        const promises = [];
+        userFollowedTab.forEach((element) => {
+          promises.push(MessageModel.find({ authorId: element, like: userId }));
+        });
+        Promise.all(promises).then((data2) => {
+          let fullData = [];
+          data2.forEach((element) => {
+            fullData = fullData.concat(element);
+          });
+          if (fullData.length === 0) {
+            resolve(null);
+          } else {
+            fullData.sort((a, b) => b.timestamp - a.timestamp);
+            resolve(fullData.slice((offset * 999) + offset, (offset + 1) * 999));
+          }
+        });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  }),
+  createMessage: ({ authorId, content }) => new Promise((resolve, reject) => {
+    const newMessage = new MessageModel({ content, authorId });
+    newMessage.save((err, message) => {
+      const { id } = message;
+      UserModel.updateOne({ _id: authorId }, { $addToSet: { messages: id } })
+        .then(() => {
+          resolve(message);
+        })
+        .catch((err) => {
+          reject(err);
+        });
     });
-  },
-  unfollow: ({targetId, userId}) => {
-    return new Promise((resolve,reject) => {
-      const promises = []
-     //remove the user to the followers of the target
-     promises.push(UserModel.updateOne({_id: targetId}, {$pull: {followers: userId}}))
-     //remove the target to the followed list of the current user
-     promises.push(UserModel.updateOne({_id : userId}, {$pull : {following: targetId}}))
-     Promise.all(promises)
-     .then(res => {
-       resolve(true)
-     })
-     .catch(err => {
-       reject(err)
-     })
-     })
-  },
-  hasFollow: ({targetId, userId}) => {
-    return new Promise((resolve,reject) => {
-      //add the user to the likes of the message
-      UserModel.findOne({_id: targetId}, {followers : 1})
-      .then(res => {
-          resolve(res.followers.includes(userId));
+  }),
+  deleteMessage: ({ messageId, authorId }) => new Promise((resolve, reject) => {
+    MessageModel.deleteOne({ _id: ObjectId(messageId) })
+      .then(() => {
+        UserModel.updateOne(
+          { _id: authorId },
+          { $pull: { messages: { id: ObjectId(messageId) } } }, false,
+        )
+          .then(() => {
+            resolve(true);
+          });
       })
-      .catch(err => {
+      .catch((err) => {
         reject(err);
+      });
+  }),
+  follow: ({ targetId, userId }) => new Promise((resolve, reject) => {
+    const promises = [];
+    // add the user to the followers of the target
+    promises.push(UserModel.updateOne({ _id: targetId }, { $addToSet: { followers: userId } }));
+    // add the target to the followed list of the current user
+    promises.push(UserModel.updateOne({ _id: userId }, { $addToSet: { following: targetId } }));
+    Promise.all(promises)
+      .then(() => {
+        resolve(true);
       })
-    })
-  },
-  like: ({messageId, userId}) => {
-    return new Promise((resolve,reject) => {
-      //add the user to the likes of the message
-      MessageModel.updateOne({_id: messageId}, {$addToSet: {like: userId}})
-      .then(res => {
-          resolve(true);
-      })
-      .catch(err => {
+      .catch((err) => {
         reject(err);
+      });
+  }),
+  unfollow: ({ targetId, userId }) => new Promise((resolve, reject) => {
+    const promises = [];
+    // remove the user to the followers of the target
+    promises.push(UserModel.updateOne({ _id: targetId }, { $pull: { followers: userId } }));
+    // remove the target to the followed list of the current user
+    promises.push(UserModel.updateOne({ _id: userId }, { $pull: { following: targetId } }));
+    Promise.all(promises)
+      .then(() => {
+        resolve(true);
       })
-    })
-  },
-  unlike : ({messageId, userId}) => {
-    return new Promise((resolve, reject) => {
-      //remove the user to the likes of the message
-      MessageModel.updateOne({_id: messageId}, {$pull: {like: userId}})
-      .then(res => {
-          resolve(true);
-      })
-      .catch(err => {
+      .catch((err) => {
         reject(err);
+      });
+  }),
+  hasFollow: ({ targetId, userId }) => new Promise((resolve, reject) => {
+    // add the user to the likes of the message
+    UserModel.findOne({ _id: targetId }, { followers: 1 })
+      .then((res) => {
+        resolve(res.followers.includes(userId));
       })
-    })
-  },
-  hasLike: ({messageId, userId}) => {
-    return new Promise((resolve, reject) => {
-      //add the user to the likes of the message
-      MessageModel.findOne({_id: messageId}, {like : 1})
-      .then(res => {
-          resolve(res.like.includes(userId));
-      })
-      .catch(err => {
+      .catch((err) => {
         reject(err);
+      });
+  }),
+  like: ({ messageId, userId }) => new Promise((resolve, reject) => {
+    // add the user to the likes of the message
+    MessageModel.updateOne({ _id: messageId }, { $addToSet: { like: userId } })
+      .then(() => {
+        resolve(true);
       })
-    })
-  },
-  createUser: ({ username, password, email }) => {
-    return new Promise((resolve, reject) => {
-      let newUser = new UserModel({ username, password, email });
-      UserModel.findOne({email}, {password: 0})
-      .then(data => {
-        if(data === null){
+      .catch((err) => {
+        reject(err);
+      });
+  }),
+  unlike: ({ messageId, userId }) => new Promise((resolve, reject) => {
+    // remove the user to the likes of the message
+    MessageModel.updateOne({ _id: messageId }, { $pull: { like: userId } })
+      .then(() => {
+        resolve(true);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  }),
+  hasLike: ({ messageId, userId }) => new Promise((resolve, reject) => {
+    // add the user to the likes of the message
+    MessageModel.findOne({ _id: messageId }, { like: 1 })
+      .then((res) => {
+        resolve(res.like.includes(userId));
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  }),
+  createUser: ({ username, password, email }) => new Promise((resolve, reject) => {
+    const newUser = new UserModel({ username, password, email });
+    UserModel.findOne({ email }, { password: 0 })
+      .then((data) => {
+        if (data === null) {
           newUser.save()
-          .then(data => {
-            newUser.password = null;
-            resolve(newUser);
-          })
-          .catch(err => {
-            reject(err)
-          })
-          }
-          else {
-            resolve(null)
-          }
-      })
-      .catch(err => {
-        reject(err);
-      })
-    })
-  },
-  getUser: ({ userId }) => {
-    return new Promise((resolve,reject) => {
-      UserModel.findOne({ _id : userId }, {password : 0})
-      .then(data => {
-        resolve(data)
-      })
-      .catch(err => {
-        reject(err);
-      })
-    })
-  },
-  getUserByEmail: ({ email }) => {
-    return new Promise((resolve,reject) => {
-      UserModel.findOne({ email }, {password : 0})
-      .then(data => {
-        resolve(data)
-      })
-      .catch(err => {
-        reject(err);
-      })
-    })
-  },
-  getUsersByIds: ({ ids }) => {
-    return new Promise((resolve,reject) => {
-      const promises = []
-      ids.forEach(id => {
-        promises.push(UserModel.findOne({_id : id}, {password : 0}));
-      })
-      Promise.all(promises)
-      .then(data => {
-        resolve(data)
-      })
-      .catch(err => {
-        reject(err);
-      })
-    })
-  },
-  getFollowers: ({userId, offset}) => {
-    return new Promise((resolve,reject) => {
-      UserModel.findOne({_id : userId}, {followers : 1})
-      .then(data => {
-        if(data === undefined || data === null || data.length === 0){
-          resolve([])
+            .then(() => {
+              newUser.password = null;
+              resolve(newUser);
+            })
+            .catch((err) => {
+              reject(err);
+            });
+        } else {
+          resolve(null);
         }
-        else{
-          const promises = []
-          const followers = data.followers
-          followers.forEach(element => {
-            promises.push(UserModel.findOne({_id : element}, {password : 0}))
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  }),
+  getUser: ({ userId }) => new Promise((resolve, reject) => {
+    UserModel.findOne({ _id: userId }, { password: 0 })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  }),
+  getUserByEmail: ({ email }) => new Promise((resolve, reject) => {
+    UserModel.findOne({ email }, { password: 0 })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  }),
+  getUsersByIds: ({ ids }) => new Promise((resolve, reject) => {
+    const promises = [];
+    ids.forEach((id) => {
+      promises.push(UserModel.findOne({ _id: id }, { password: 0 }));
+    });
+    Promise.all(promises)
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  }),
+  getFollowers: ({ userId, offset }) => new Promise((resolve, reject) => {
+    UserModel.findOne({ _id: userId }, { followers: 1 })
+      .then((data) => {
+        if (data === undefined || data === null || data.length === 0) {
+          resolve([]);
+        } else {
+          const promises = [];
+          const { followers } = data;
+          followers.forEach((element) => {
+            promises.push(UserModel.findOne({ _id: element }, { password: 0 }));
           });
           Promise.all(promises)
-          .then(result => {
-            resolve(result.slice((offset*999) + offset, (offset + 1) * 999))
-          })
-          .catch(err => {
-            resolve(err);
-          })
+            .then((result) => {
+              resolve(result.slice((offset * 999) + offset, (offset + 1) * 999));
+            })
+            .catch((err) => {
+              resolve(err);
+            });
         }
       })
-      .catch(err => {
-        reject(err)
-      })
-    })
-  },
-  getFollowings: ({userId, offset}) => {
-    return new Promise((resolve,reject) => {
-      UserModel.findOne({_id : userId}, {following : 1})
-      .then(data => {
-        if(data === undefined || data === null || data.length === 0){
-          resolve([])
-        }
-        else{
-          const promises = []
-          const following = data.following
-          following.forEach(element => {
-            promises.push(UserModel.findOne({_id : element}, {password : 0}))
+      .catch((err) => {
+        reject(err);
+      });
+  }),
+  getFollowings: ({ userId, offset }) => new Promise((resolve, reject) => {
+    UserModel.findOne({ _id: userId }, { following: 1 })
+      .then((data) => {
+        if (data === undefined || data === null || data.length === 0) {
+          resolve([]);
+        } else {
+          const promises = [];
+          const { following } = data;
+          following.forEach((element) => {
+            promises.push(UserModel.findOne({ _id: element }, { password: 0 }));
           });
           Promise.all(promises)
-          .then(result => {
-            resolve(result.slice((offset*999) + offset, (offset + 1) * 999))
-          })
-          .catch(err => {
-            resolve(err);
-          })
+            .then((result) => {
+              resolve(result.slice((offset * 999) + offset, (offset + 1) * 999));
+            })
+            .catch((err) => {
+              resolve(err);
+            });
         }
       })
-      .catch(err => {
-        reject(err)
-      })
-    })
-  },
-  searchUser: ({pattern}) => {
-    return new Promise((resolve,reject) => {
-      UserModel.find({"username" : {'$regex': pattern}}, {password : 0})
-      .then(data => {
-        if(data.length === undefined || data.length === 0){
-          resolve([])
+      .catch((err) => {
+        reject(err);
+      });
+  }),
+  searchUser: ({ pattern }) => new Promise((resolve, reject) => {
+    UserModel.find({ username: { $regex: pattern } }, { password: 0 })
+      .then((data) => {
+        if (data.length === undefined || data.length === 0) {
+          resolve([]);
         }
-          resolve(data.slice(0,999))
+        resolve(data.slice(0, 999));
       })
-      .catch(err => {
-        reject(err)
-      })
-    })
-  },
-  changeImage: ({userId, mood}) => {
-    return new Promise((resolve,reject) => {
-      UserModel.updateOne({_id : userId}, {image : images[mood][Math.floor(Math.random() * images[mood].length)]})
-      .then(data => {
-        result = data
-        result.password = "";
+      .catch((err) => {
+        reject(err);
+      });
+  }),
+  changeImage: ({ userId, mood }) => new Promise((resolve, reject) => {
+    UserModel.updateOne(
+      { _id: userId },
+      { image: images[mood][Math.floor(Math.random() * images[mood].length)] },
+    )
+      .then((data) => {
+        const result = data;
+        result.password = '';
         resolve(result);
       })
-      .catch(err => {
-        reject(err)
-      })
-    })
-  }
+      .catch((err) => {
+        reject(err);
+      });
+  }),
 };
 
 module.exports = { schema, root };
